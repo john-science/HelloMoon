4590fc81f5a27da904b62f024af824228924caad
theJollySin <thejollysin@gmail.com>
2015-05-06 21:29:32 -0700

    finished ch 13

--- app/src/main/java/net/antineutrino/android/hellomoon/AudioPlayer.java
+++ app/src/main/java/net/antineutrino/android/hellomoon/AudioPlayer.java
@@ -1,7 +1,29 @@
 package net.antineutrino.android.hellomoon;
 
-/**
- * Created by thejollysin on 5/6/15.
- */
+import android.content.Context;
+import android.media.MediaPlayer;
+
 public class AudioPlayer {
+
+    private MediaPlayer mPlayer;
+
+    public void stop() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    public void play(Context c) {
+        stop();
+        mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                stop();
+            }
+        });
+
+        mPlayer.start();
+    }
 }
--- app/src/main/java/net/antineutrino/android/hellomoon/HelloMoonFragment.java
+++ app/src/main/java/net/antineutrino/android/hellomoon/HelloMoonFragment.java
@@ -2,16 +2,18 @@ package net.antineutrino.android.hellomoon;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class HelloMoonFragment extends Fragment {
+
+    private Button mPlayButton;
+    private Button mStopButton;
+    private AudioPlayer mPlayer = new AudioPlayer();
 
 
     public HelloMoonFragment() {
@@ -20,11 +22,30 @@ public class HelloMoonFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hello_moon, container, false);
+        View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
+
+        mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mPlayer.play(getActivity());
+            }
+        });
+        mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mPlayer.stop();
+            }
+        });
+
+        return v;
     }
 
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPlayer.stop();
+    }
 }
