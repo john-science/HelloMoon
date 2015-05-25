281040fb355be69ae483766966a67c9beafe36f1
theJollySin <thejollysin@gmail.com>
2015-05-25 15:46:56 -0700

    close #2 unusable buttons are graying out

--- app/src/main/java/net/antineutrino/android/hellomoon/HelloMoonFragment.java
+++ app/src/main/java/net/antineutrino/android/hellomoon/HelloMoonFragment.java
@@ -1,6 +1,7 @@
 package net.antineutrino.android.hellomoon;
 
 
+import android.annotation.TargetApi;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -31,28 +32,53 @@ public class HelloMoonFragment extends Fragment {
         setRetainInstance(true);
     }
 
+    @TargetApi(16)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
 
         mPlayButton = (ImageButton)v.findViewById(R.id.hellomoon_playButton);
+        mPlayButton.setEnabled(true);
+        mPlayButton.setImageAlpha(1000);
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                mPlayButton.setImageAlpha(100);
+                mPauseButton.setImageAlpha(1000);
+                mStopButton.setImageAlpha(1000);
+                mPlayButton.setEnabled(false);
+                mPauseButton.setEnabled(true);
+                mStopButton.setEnabled(true);
                 mPlayer.play(getActivity());
             }
         });
 
         mPauseButton = (ImageButton)v.findViewById(R.id.hellomoon_pauseButton);
+        mPauseButton.setEnabled(false);
+        mPauseButton.setImageAlpha(100);
         mPauseButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                mPlayButton.setImageAlpha(1000);
+                mPauseButton.setImageAlpha(100);
+                mStopButton.setImageAlpha(1000);
+                mPlayButton.setEnabled(true);
+                mPauseButton.setEnabled(false);
+                mStopButton.setEnabled(true);
                 mPlayer.pause();
             }
         });
 
         mStopButton = (ImageButton)v.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setEnabled(false);
+        mStopButton.setImageAlpha(100);
         mStopButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                mPlayButton.setImageAlpha(1000);
+                mPauseButton.setImageAlpha(100);
+                mStopButton.setImageAlpha(100);
+                mPlayButton.setEnabled(true);
+                mPauseButton.setEnabled(false);
+                mStopButton.setEnabled(false);
                 mPlayer.stop();
             }
         });
